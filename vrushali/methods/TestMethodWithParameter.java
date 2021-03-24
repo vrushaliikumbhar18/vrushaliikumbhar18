18eb3e9add4568324907d1490cdb082e5bb1f991
Vrushali <vrushalii.kumbhar18@gmail.com>
2021-03-24 20:34:11 +0530

    done

---
+++ vrushali/methods/TestMethodWithParameter.java
@@ -0,0 +1,8 @@
+package com.techsum.vrushali.methods;
+
+public class TestMethodWithParameter {
+
+    public static void main(String[] args) {
+        System.out.println("Hello TestMethodWithParameter");
+    }
+}
---
+++ vrushali/variables/TestVariables.java
@@ -0,0 +1,7 @@
+package com.techsum.vrushali.variables;
+
+public class TestVariables {
+    public static void main(String[] args) {
+        System.out.println("Hello TestVariables");
+    }
+}
